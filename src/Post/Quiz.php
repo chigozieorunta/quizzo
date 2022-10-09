723b90487c16dbf6d78236590d0bb2080853a81c
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-10-09 13:04:30 +0100

    Added new methods to Quiz class

--- src/Post/Quiz.php
+++ src/Post/Quiz.php
@@ -9,4 +9,22 @@ class Quiz extends AbstractPost {
 	 * @var string
 	 */
 	public static $name = 'quiz';
+
+	/**
+	 * Return singular label
+	 *
+	 * @return string
+	 */
+	public function get_singular_label(): string {
+		return 'Quiz';
+	}
+
+	/**
+	 * Return plural label
+	 *
+	 * @return string
+	 */
+	public function get_plural_label(): string {
+		return 'Quizzes';
+	}
 }
