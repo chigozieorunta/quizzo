724a23f80d5c5805a2f3a9b0874f41ed6a37f7e0
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-10-11 14:19:54 +0100

    Added MetaBox factory

---
+++ src/MetaBox/MetaBoxFactory.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Meta box factory.
+ *
+ * @package Quizzo
+ */
+
+namespace Quizzo\MetaBox;
+
+use RuntimeException;
+
+/**
+ * Meta box factory builds the Quizzo meta box class instances.
+ */
+class MetaBoxFactory {
+	/**
+	 * Meta boxes
+	 *
+	 * @var array
+	 */
+	private static array $meta_boxes = [
+		'Answer' => 'Answer'
+	];
+
+	/**
+	 * Set up the Quizzo meta boxes.
+	 *
+	 * @return void
+	 */
+	public static function init(): void {
+		foreach( self::$meta_boxes as $meta_box ) {
+			self::create( $meta_box );
+		}
+	}
+
+	/**
+	 * Create meta box.
+	 *
+	 * @param string $meta_box
+	 * @return void
+	 */
+	public static function create(string $meta_box): void {
+		$class = __NAMESPACE__ . '\\' . $post_type;
+		if ( class_exists( $class ) ) {
+			$object = new $class();
+			$object->register_meta_box();
+		} else {
+			throw new RuntimeException( $class . ' does not exist.' );
+		}
+	}
+}
