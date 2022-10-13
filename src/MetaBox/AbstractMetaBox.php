b80c00b2241a518b9f5eaeb9f13b4bd3191728dc
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-10-13 14:17:10 +0100

    Added logic implementation for AbstractMetaBox

---
+++ src/MetaBox/AbstractMetaBox.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Meta box abstraction
+ *
+ * @package Quizzo
+ */
+
+namespace Quizzo\MetaBox;
+
+use LogicException;
+
+/**
+ * Base Handler class for registering Meta boxes
+ *
+ */
+abstract class AbstractMetaBox {
+
+	/**
+	 * Abstract Meta box.
+	 *
+	 * @return void
+	 *
+	 * @throws \LogicException If $name property is not statically defined within child classes.
+	 */
+	final public function __construct() {
+		if ( ! isset( static::$name ) ) {
+			throw new LogicException( __CLASS__ . ' must define static property name' );
+		}
+	}
+
+	/**
+	 * Get Meta box name.
+	 *
+	 * @return string
+	 */
+	public function get_name(): string {
+		return static::$name;
+	}
+
+	/**
+	 * Get Meta box heading.
+	 *
+	 * @return string
+	 */
+	abstract public function get_heading(): string;
+
+	/**
+	 * Get Meta box parent post type.
+	 *
+	 * @return string
+	 */
+	abstract public function get_post_type(): string;
+
+	/**
+	 * Get Meta box callback.
+	 *
+	 * @return string
+	 */
+	abstract public function get_metabox_callback(): string;
+
+	/**
+	 * Register meta box.
+	 *
+	 * @return void
+	 */
+	public function register_meta_box(): void {
+		add_meta_box(
+			PLUGIN_SLUG . '_' . $this->get_name(),
+			__( $this->get_heading(), PLUGIN_DOMAIN ),
+			[ $this, 'get_metabox_callback' ],
+			$this->get_post_type()
+		);
+	}
+}
