3d31a4465173ed772cb8aae2ff5667f07d7f544b
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-10-15 20:50:21 +0100

    Added Menu and Assets class to hooks

--- src/Quizzo.php
+++ src/Quizzo.php
@@ -40,6 +40,8 @@ class Quizzo {
 	public function activate(): void {
 		add_action( 'init', [ $this, 'register_post_types' ] );
 		add_action( 'add_meta_boxes', [ $this, 'register_meta_boxes' ] );
+		add_action( 'admin_menu', [ $this, 'register_menu' ], 9 );
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_assets' ] );
 	}
 
 	/**
@@ -65,6 +67,34 @@ class Quizzo {
 			MetaBox\MetaBoxFactory::init();
 		} catch ( Exception $e ) {
 			wp_die( 'Error: Running Meta Box Factory - ' . $e->getMessage() );
+		}
+	}
+
+	/**
+	 * Register Menu.
+	 *
+	 * @return void
+	 */
+	public function register_menu(): void {
+		try {
+			$menu = Plugin\Menu::get_instance();
+			$menu->init();
+		} catch ( Exception $e ) {
+			wp_die( 'Error: Registering Menu - ' . $e->getMessage() );
+		}
+	}
+
+	/**
+	 * Register Assets.
+	 *
+	 * @return void
+	 */
+	public function register_assets(): void {
+		try {
+			$assets = Plugin\Assets::get_instance();
+			$assets->init();
+		} catch ( Exception $e ) {
+			wp_die( 'Error: Registering Assets - ' . $e->getMessage() );
 		}
 	}
 }
