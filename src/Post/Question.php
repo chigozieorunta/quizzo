3bc010656ec7d534c8139597196d0df6492df279
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-11-08 10:46:01 +0100

    Added Question class for Question post type

--- src/Post/Question.php
+++ src/Post/Question.php
@@ -3,5 +3,28 @@
 namespace Quizzo\Post;
 
 class Question extends AbstractPost {
+	/**
+	 * Post type
+	 *
+	 * @var string
+	 */
+	public static $name = 'question';
 
+	/**
+	 * Return singular label
+	 *
+	 * @return string
+	 */
+	public function get_singular_label(): string {
+		return 'Question';
+	}
+
+	/**
+	 * Return plural label
+	 *
+	 * @return string
+	 */
+	public function get_plural_label(): string {
+		return 'Questions';
+	}
 }
