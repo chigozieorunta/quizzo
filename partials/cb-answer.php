6766b1c9433f095c89f6b2370c25072cb615dff6
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-11-10 13:32:03 +0100

    Removed callback method for Answer MetaBox

--- partials/cb-answer.php
+++
@@ -1,9 +0,0 @@
-<?php global $quiz_options, $quiz_answer; ?>
-
-<select class="widefat" name="quizzo_answer">
-	<?php foreach ( $quiz_options as $key => $value ) : ?>
-		<option value="<?php echo esc_attr( $key ); ?>" <?php selected( $quiz_answer, esc_attr( $key ) ); ?>>
-			<?php echo esc_html( $value ); ?>
-		</option>
-	<?php endforeach; ?>
-</select>
