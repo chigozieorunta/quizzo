e99baa2600f371e34e397cf8e68a2c2df8bc2dac
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-11-15 10:45:27 +0100

    Added Score class for Score post type

--- src/Post/Score.php
+++ src/Post/Score.php
@@ -3,5 +3,28 @@
 namespace Quizzo\Post;
 
 class Score extends AbstractPost {
+	/**
+	 * Post type
+	 *
+	 * @var string
+	 */
+	public static $name = 'score';
 
+	/**
+	 * Return singular label
+	 *
+	 * @return string
+	 */
+	public function get_singular_label(): string {
+		return 'Score';
+	}
+
+	/**
+	 * Return plural label
+	 *
+	 * @return string
+	 */
+	public function get_plural_label(): string {
+		return 'Scores';
+	}
 }
